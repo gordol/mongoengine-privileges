91971aadfc2cb09f8ed712fbbebb567380347873
Marcel van den Elst <marcel@marutec.nl>
2012-06-28 13:18:58 +0200

    improved setup and imports

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python
-from distutils.core import setup
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires=[
+    'mongoengine',
+    'mongoengine_relational',
+    'pyramid',
+    'inspect',
+]
 
 setup(
     name = 'mongoengine-privileges',
-    description = '''Mixin for MongoEngine that manages object-level privileges.''',
     version = 0.1,
+    description = '''Mixin for MongoEngine that manages object-level privileges.''',
+    long_description = README,
     author = 'Paul Uithol - Progressive Company',
     author_email = 'paul.uithol@progressivecompany.nl',
     url = 'http://github.com/ProgressiveCompany/mongoengine-privileges',
-    packages=['mongoengine_privileges'],
-    requires=[
-        'mongoengine',
-        'mongoengine_relational',
-        'pyramid',
-        'inspect',
-    ],
-    install_requires=[
-        'mongoengine',
-        'mongoengine_relational',
-        'pyramid',
-        'inspect',
-    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    requires=requires,
+    install_requires=requires,
+    tests_require=requires,
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
